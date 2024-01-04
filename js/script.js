02c2dbfc4cec58098db0339f17489b2978c03cf5
Er4ld0 <junior-eraldo2011@hotmail.com>
2024-01-03 22:24:08 -0300

    git commit -m

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var span = document.getElementsByTagName('menu__items > span');
+
+function openMenu() {
+    if (item.style.display == 'flex') {
+        item.style.display = 'none'
+    } else {
+        item.style.display = 'flex'
+    }
+
+    item.style.backgroundColor = 'rgba(0, 0, 0, 0.568)'
+}
